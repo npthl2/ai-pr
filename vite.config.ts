228dfccdc8f087c7aa1e9938afe4089e3cb33dd6
Teddy-22 <taheelee22@lgcns.com>
2025-01-23 19:38:10 +0900

    chore: 로컬개발환경을 위한 프록시 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -19,4 +19,14 @@ export default defineConfig({
       '@layout': path.resolve(__dirname, './src/layout'),
     },
   },
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        secure: false,
+      },
+    },
+  },
+});
