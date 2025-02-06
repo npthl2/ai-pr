e0297e87c5b23ffaaa191f8c2e969d21787eafb0
Hekatones <Taesik.Shin@lgcns.com>
2025-02-06 17:47:33 +0900

    middle commit

---
+++ test/unit/specs/pages/examples/login/components/LoginForm.spec.tsx
@@ -0,0 +1,8 @@
+import { describe, it, expect } from 'vitest';
+import LoginForm from '../../../../../../../src/pages/examples/login/components/LoginForm';
+
+describe('LoginForm', () => {
+  it('should render', () => {
+    expect(LoginForm).toBeDefined();
+  });
+});
