53056abce8a6fef1ba93156ede5b03252e8e0af9
Teddy-22 <taheelee22@lgcns.com>
2025-02-20 10:31:10 +0900

    feat: npm install 필요. Zustand 미들웨어 추가
    simple-zustand-devtools 추가
    스토어별 설정을 위한 코드 추가

--- src/stores/AuthStore.ts
+++ src/stores/AuthStore.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { mountStoreDevtool } from 'simple-zustand-devtools';
 
 interface AuthState {
   isAuthenticated: boolean;
@@ -11,5 +12,9 @@ const useAuthStore = create<AuthState>((set) => ({
   login: () => set({ isAuthenticated: true }),
   logout: () => set({ isAuthenticated: false }),
 }));
+
+if (import.meta.env.DEV) {
+  mountStoreDevtool('Auth Store', useAuthStore); // 개발도구에 노출될 적절한 문자열을 지정해 주세요.
+}
 
 export default useAuthStore;
--- src/stores/dialogStore.ts
+++ src/stores/dialogStore.ts
@@ -1,5 +1,6 @@
 import { create } from 'zustand';
 import { DialogOptions } from '@model/Dialog';
+import { mountStoreDevtool } from 'simple-zustand-devtools';
 
 interface DialogState {
   dialogs: {
@@ -33,3 +34,7 @@ export const useDialogStore = create<DialogState>()((set) => ({
       dialogs: state.dialogs.filter((dialog) => dialog.id !== id),
     })),
 }));
+
+if (import.meta.env.DEV) {
+  mountStoreDevtool('Dialog Store', useDialogStore); // 개발도구에 노출될 적절한 문자열을 지정해 주세요.
+}
