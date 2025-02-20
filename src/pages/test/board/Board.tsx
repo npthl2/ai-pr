60ccb6081acfb8c7624641cb928149abe12c9b3b
Hekatones <Taesik.Shin@lgcns.com>
2025-02-20 18:09:00 +0900

    adjust lint

--- src/pages/test/board/Board.tsx
+++ src/pages/test/board/Board.tsx
@@ -16,39 +16,6 @@ const Board: React.FC = () => {
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
-  const data = [
-    {
-      id: 1,
-      title: '첫 번째 게시글',
-      content: '이것은 첫 번째 게시글의 내용입니다.',
-      createdAt: '2025-02-01',
-    },
-    {
-      id: 2,
-      title: '두 번째 게시글',
-      content: '두 번째 게시글의 내용이 여기에 들어갑니다.',
-      createdAt: '2025-02-02',
-    },
-    {
-      id: 3,
-      title: '세 번째 게시글',
-      content: '세 번째 게시글에 대한 내용이 여기에 있습니다.',
-      createdAt: '2025-02-03',
-    },
-    {
-      id: 4,
-      title: '네 번째 게시글',
-      content: '네 번째 게시글의 내용이 여기에 들어가 있습니다.',
-      createdAt: '2025-02-04',
-    },
-    {
-      id: 5,
-      title: '다섯 번째 게시글',
-      content: '다섯 번째 게시글의 내용입니다. 여기에 다양한 정보가 포함될 수 있습니다.',
-      createdAt: '2025-02-05',
-    },
-  ];
-
   const navigate = useNavigate();
 
   const handleAddPostClick = () => {
@@ -62,8 +29,8 @@ const Board: React.FC = () => {
         if (response.successOrNot === 'Y' && response.data) {
           setBoardList(response.data as unknown as BoardItemType[]);
         }
-        // setBoardList(data);
       } catch (error) {
+        console.error('Failed to load board data', error);
         setError('Failed to load board data');
       } finally {
         setLoading(false);
