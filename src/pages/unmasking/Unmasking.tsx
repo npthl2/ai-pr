51392d0503babf9cf7bdd3892bb67da2512268f1
lsj-JayL <lsj.dummy@gmail.com>
2025-03-03 21:57:01 +0900

    27-unmasking-날짜포맷지정

--- src/pages/unmasking/Unmasking.tsx
+++ src/pages/unmasking/Unmasking.tsx
@@ -8,6 +8,7 @@ import { UnmaskingRequestDto, UnmaskingResponseDto, UnmaskingProps } from '@mode
 import unmaskingService from '@api/services/unmaskingService';
 import useCustomerStore from '@stores/CustomerStore';
 import useMemberStore from '@stores/MemberStore';
+import { format } from 'date-fns';
 
 const CharCount = styled(Typography)({
   position: 'absolute',
@@ -31,18 +32,7 @@ const Unmasking = <T,>({ onClose, onUnmask, requestData }: UnmaskingProps<T>) =>
 
     return () => clearTimeout(timer);
   }, []); // Empty dependency array means this runs once on mount
-  /*
-  useEffect(() => {
-    if (inputTextRef.current) {
-      inputTextRef.current.focus();
-    }
-  }, []);
-  TextField의 포커스가 작동하지 않는 문제는 Dialog가 열린 후에 포커스를 설정해야 하기 때문일 수 있습니다. useEffect에 약간의 지연을 추가해보겠습니다:
-  setTimeout을 사용하여 Dialog가 완전히 렌더링된 후에 포커스를 설정합니다.
-cleanup 함수를 추가하여 컴포넌트가 언마운트될 때 타이머를 정리합니다.
-기존의 autoFocus 속성은 유지합니다.
-이렇게 하면 Dialog가 열린 후 TextField에 자동으로 포커스가 될 것입니다.
-  */
+
 
   const handleReasonChange = (value: string) => {
     if (value.length <= 50) {
@@ -64,10 +54,10 @@ cleanup 함수를 추가하여 컴포넌트가 언마운트될 때 타이머를 
     const unmaskingRequestDto: UnmaskingRequestDto = {
       ...requestData,
       requestUnmaskingReason: reason, // key in 사유
-      requestUnmaskingDatetime : new Date().toISOString(),//'2025-02-26 10:00:00', // 요청일시
-      requestMemberId: memberId || 'S-0001', // 로그인 ID -> kyle 님이 로그인 사용자 정보 저장한거를 꺼내오면 됨
-      requestMemberConnectedIp: '10.231.58.61',// 이건 시간되면 local ip 가져와서 채우는 거 넣어보자
-      customerId: selectedCustomerId || 'cust1234567890', // 고객검색하면 현재 선택된 고객의 ID. CustomerStore에서 selectedCustomerId를 꺼내오면 됨
+      requestUnmaskingDatetime : format(new Date(), 'yyyy-MM-dd HH:mm:ss'),
+      requestMemberId: memberId || 'S-0001', // MemberStore에서 memberId -> common 에서 X-Authorization-Id 사용하는 걸로 변경 예정.. 나중에.....
+      requestMemberConnectedIp: '10.231.58.61',// common 에서 X-Authorization-Id 사용하는 걸로 변경 예정.. 나중에.....
+      customerId: selectedCustomerId || 'cust1234567890', // CustomerStore에서 selectedCustomerId
     };
 
     try {
