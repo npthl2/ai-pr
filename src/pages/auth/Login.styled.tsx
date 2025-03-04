60986ee423ca69bf5aea8398c0ba97070cb731cb
개상 <gs@gaesang-ui-MacBookPro.local>
2025-03-04 18:07:16 +0900

    testfield 변경

--- src/pages/auth/Login.styled.tsx
+++ src/pages/auth/Login.styled.tsx
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
-import { Typography, Box } from '@mui/material';
+import { Typography, Box, Button } from '@mui/material';
 import { grey } from '@mui/material/colors';
 import { Theme } from '@mui/material';
 // import type { Theme } from '@mui/material/styles';
@@ -26,7 +26,7 @@ export const FormContainer = styled(Box)({
     width: '400px',
     margin: '126.5px auto',
     padding: '40px 32px',
-    gap: '24px',
+    gap: '6px',
     backgroundColor: grey[50],
     border: `1px solid ${grey[500]}`,
     borderRadius: '10px',
@@ -45,7 +45,7 @@ export const Title = styled(Typography)(({ theme }: { theme: Theme }) => ({
     fontSize: '16px', // 폰트 크기
     fontWeight: 300, // 글자 굵기를 700으로 설정 (Bold 효과)
     letterSpacing: '1px', // 글자 사이 간격을 0.5px로 설정
-    margin: 0, // 여백 제거
+
 }));
 
 export const Subtitle = styled(Typography)(({ theme }: { theme: Theme }) => ({
@@ -53,16 +53,48 @@ export const Subtitle = styled(Typography)(({ theme }: { theme: Theme }) => ({
     fontSize: '30px', // 폰트 크기
     fontWeight: 300, // 글자 굵기를 700으로 설정 (Bold 효과)
     letterSpacing: '1px', // 글자 사이 간격을 0.5px로 설정
-    margin: 0, // 여백 제거
+    marginBottom: 20, // 여백 제거
 }));
 
 export const AlertContainer = styled(Box)`
-    margin-bottom: 24px;
+    margin-bottom: 12px;
 `;
 
 export const FormFieldsContainer = styled(Box)`
     display: flex;
     flex-direction: column;
-    gap: 24px;
+    gap: 12px;
     margin-bottom: 24px;
+`;
+
+export const LoginIdContainer = styled(Box)`
+    display: flex;
+    flex-direction: column;
+    gap: 6px;
+    margin-bottom: 6px;
+`;
+
+export const LoginPasswordContainer = styled(Box)`
+    display: flex;
+    flex-direction: column;
+    gap: 6px;
+`;
+
+export const ContainerTitle = styled(Typography)(({ theme }: { theme: Theme }) => ({
+    color: theme.palette.common.black,
+    fontSize: '16px', // 폰트 크기
+    fontWeight: 600, // 글자 굵기를 700으로 설정 (Bold 효과)
+    letterSpacing: '0.5px', // 글자 사이 간격을 0.5px로 설정
+    margin: 0, // 여백 제거
+}));
+
+export const LoginButton = styled(Button)`
+    background-color: ${grey[900]};
+    text-transform: none;
+    margin-bottom: 36px;
+    color: white;
+
+    &:hover {
+        background-color: ${grey[800]};
+    }
 `;
--- src/pages/auth/components/LoginForm.tsx
+++ src/pages/auth/components/LoginForm.tsx
@@ -1,11 +1,104 @@
-import { TextField, Button, IconButton, InputAdornment } from '@mui/material';
-import { Visibility, VisibilityOff } from '@mui/icons-material';
-import { FormFieldsContainer } from '../Login.styled';
-import { useState } from 'react';
+// import { TextField, Button, IconButton, InputAdornment } from '@mui/material';
+// import { Visibility, VisibilityOff } from '@mui/icons-material';
+// import { FormFieldsContainer } from '../Login.styled';
+// import { useState } from 'react';
+// import { LoginRequestParams } from '@model/Auth';
+// import LoginAlert from './LoginAlert';
+// import { LoginError } from '../Login.model';
+
+// interface LoginFormProps {
+//     formData: LoginRequestParams;
+//     isLoading: boolean;
+//     errors: LoginError;
+//     onSubmit: () => void;
+//     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+//     onBlur: (e: React.FocusEvent<HTMLInputElement>) => void;
+// }
+
+// const LoginForm = ({ formData, isLoading, errors, onSubmit, onChange, onBlur }: LoginFormProps) => {
+//     const [showPassword, setShowPassword] = useState(false);
+
+//     const handleTogglePassword = () => {
+//         setShowPassword((prev) => !prev);
+//     };
+
+//     return (
+//         <>
+//             <FormFieldsContainer>
+//                 <TextField
+//                     label="ID"
+//                     name="loginId"
+//                     autoFocus
+//                     value={formData.loginId}
+//                     onChange={onChange}
+//                     onBlur={onBlur}
+//                     disabled={isLoading}
+//                     fullWidth
+//                     error={!!errors.loginId}
+//                     helperText={errors.loginId}
+//                     data-testid="id"
+//                 />
+//                 <TextField
+//                     label="Password"
+//                     name="password"
+//                     data-testid="pw"
+//                     type={showPassword ? 'text' : 'password'}
+//                     value={formData.password}
+//                     onChange={onChange}
+//                     onBlur={onBlur}
+//                     disabled={isLoading}
+//                     fullWidth
+//                     error={!!errors.password}
+//                     helperText={errors.password}
+//                     InputProps={{
+//                         endAdornment: (
+//                             <InputAdornment position="end">
+//                                 <IconButton
+//                                     aria-label="비밀번호 보기 토글"
+//                                     onClick={handleTogglePassword}
+//                                     edge="end"
+//                                     disabled={isLoading}
+//                                     size="small"
+//                                 >
+//                                     {showPassword ? <VisibilityOff /> : <Visibility />}
+//                                 </IconButton>
+//                             </InputAdornment>
+//                         ),
+//                     }}
+//                 />
+//             </FormFieldsContainer>
+//             <LoginAlert error={errors} />
+//             <Button
+//                 variant="contained"
+//                 onClick={onSubmit}
+//                 disabled={isLoading}
+//                 fullWidth
+//                 data-testid="login"
+//                 sx={{
+//                     height: '36px',
+//                     backgroundColor: '#050E1F',
+//                     '&:hover': {
+//                         backgroundColor: '#050E1F',
+//                     },
+//                     textTransform: 'none',
+//                 }}
+//             >
+//                 {isLoading ? '로그인 중...' : '로그인'}
+//             </Button>
+//         </>
+//     );
+// };
+
+// export default LoginForm;
+
+// import { Visibility, VisibilityOff } from '@mui/icons-material';
+import { ContainerTitle, FormFieldsContainer, LoginIdContainer, LoginPasswordContainer, LoginButton } from '../Login.styled';
+// import { useState } from 'react';
 import { LoginRequestParams } from '@model/Auth';
 import LoginAlert from './LoginAlert';
 import { LoginError } from '../Login.model';
-
+import { getTheme } from '@theme/theme';
+import TextField from '@components/TextField';
 interface LoginFormProps {
     formData: LoginRequestParams;
     isLoading: boolean;
@@ -16,59 +109,63 @@ interface LoginFormProps {
 }
 
 const LoginForm = ({ formData, isLoading, errors, onSubmit, onChange, onBlur }: LoginFormProps) => {
-    const [showPassword, setShowPassword] = useState(false);
+    
+    // 비밀번호 확인 토글 관련
+    // const [showPassword, setShowPassword] = useState(false);
 
-    const handleTogglePassword = () => {
-        setShowPassword((prev) => !prev);
-    };
+    // const handleTogglePassword = () => {
+    //     setShowPassword((prev) => !prev);
+    // };
 
     return (
         <>
             <FormFieldsContainer>
-                <TextField
-                    label="ID"
-                    name="loginId"
-                    autoFocus
-                    value={formData.loginId}
-                    onChange={onChange}
-                    onBlur={onBlur}
-                    disabled={isLoading}
-                    fullWidth
-                    error={!!errors.loginId}
-                    helperText={errors.loginId}
-                    data-testid="id"
-                />
-                <TextField
+                <LoginIdContainer>
+                    <ContainerTitle theme={getTheme('light')}>ID</ContainerTitle>
+                        <TextField
+                        label="ID"
+                        name="loginId"
+                        autoFocus
+                        value={formData.loginId}
+                        onChange={(value) => onChange({ target: { name: 'loginId', value } } as React.ChangeEvent<HTMLInputElement>)}
+                        onBlur={onBlur}
+                        disabled={isLoading}
+                        fullWidth
+                        state={errors.loginId ? 'error' : 'inactive'}
+                        helperText={errors.loginId}
+                        data-testid="id"
+                        />
+                </LoginIdContainer>
+                <LoginPasswordContainer>
+                    <ContainerTitle theme={getTheme('light')}>Password</ContainerTitle>
+                    <TextField
                     label="Password"
                     name="password"
                     data-testid="pw"
-                    type={showPassword ? 'text' : 'password'}
+                    // type={showPassword ? 'text' : 'password'}
                     value={formData.password}
-                    onChange={onChange}
+                    onChange={(value) => onChange({ target: { name: 'password', value } } as React.ChangeEvent<HTMLInputElement>)}
                     onBlur={onBlur}
                     disabled={isLoading}
                     fullWidth
-                    error={!!errors.password}
+                    state={errors.password ? 'error' : 'inactive'}
                     helperText={errors.password}
-                    InputProps={{
-                        endAdornment: (
-                            <InputAdornment position="end">
-                                <IconButton
-                                    aria-label="비밀번호 보기 토글"
-                                    onClick={handleTogglePassword}
-                                    edge="end"
-                                    disabled={isLoading}
-                                    size="small"
-                                >
-                                    {showPassword ? <VisibilityOff /> : <Visibility />}
-                                </IconButton>
-                            </InputAdornment>
-                        ),
-                    }}
+                    // suffix={
+                    //     <IconButton
+                    //         aria-label={showPassword ? '비밀번호 숨기기' : '비밀번호 보기'}
+                    //         onClick={handleTogglePassword}
+                    //         edge="end"
+                    //         disabled={isLoading}
+                    //         size="small"
+                    //     >
+                    //         {showPassword ? <VisibilityOff /> : <Visibility />}
+                    //     </IconButton>
+                    // }
                 />
+                </LoginPasswordContainer>
             </FormFieldsContainer>
             <LoginAlert error={errors} />
-            <Button
+            <LoginButton
                 variant="contained"
                 onClick={onSubmit}
                 disabled={isLoading}
@@ -84,7 +181,7 @@ const LoginForm = ({ formData, isLoading, errors, onSubmit, onChange, onBlur }: 
                 }}
             >
                 {isLoading ? '로그인 중...' : '로그인'}
-            </Button>
+            </LoginButton>
         </>
     );
 };
