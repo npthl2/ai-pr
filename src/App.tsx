6f5f93e82b7ee774e820a251d7902d057a27c2c3
Teddy-22 <taheelee22@lgcns.com>
2025-03-06 15:39:27 +0900

    chore: 불필요한 예제를 위한 라우팅 제거

--- src/App.tsx
+++ src/App.tsx
@@ -15,8 +15,6 @@ import SelectExample from '@pages/examples/select/SelectExample';
 import CheckboxExample from '@pages/examples/checkbox/CheckboxExample';
 import ButtonExample from '@pages/examples/button/ButtonExample';
 import RadioExample from '@pages/examples/radio/RadioExample';
-import BoardExample from '@pages/examples/board/BoardExample';
-import LoginExample from '@pages/examples/login/LoginExample';
 import AutocompleteExample from '@pages/examples/autocomplete/AutocompleteExample';
 import ChipExample from '@pages/examples/chip/ChipExample';
 import TextFieldExample from '@pages/examples/textField/TextFieldExample';
@@ -26,7 +24,6 @@ import DialogExample from '@pages/examples/dialog/DialogExample';
 import TabsExample from '@pages/examples/tabs/TabsExample';
 import TableExample from '@pages/examples/table/TableExample';
 import ToastExample from '@pages/examples/toast/ToastExmple';
-import ApiTestExample from './pages/examples/apiTest/ApiTestExample';
 import Board from '@pages/test/board/Board';
 import RegistBoard from '@pages/test/board/component/RegistBoard';
 import MemoAndHistoryPanel from '@pages/memoAndSendHistory/MemoAndSendHistoryPanel';
@@ -42,13 +39,11 @@ function App() {
             <Route path='/login' element={<Login />} />
             <Route element={<Layout />}>
               <Route path='example'>
-                <Route path='login' element={<LoginExample />} />
                 <Route path='nestedDialog' element={<NestedDialogExample />} />
                 <Route path='select' element={<SelectExample />} />
                 <Route path='checkbox' element={<CheckboxExample />} />
                 <Route path='button' element={<ButtonExample />} />
                 <Route path='radio' element={<RadioExample />} />
-                <Route path='board' element={<BoardExample />} />
                 <Route path='autocomplete' element={<AutocompleteExample />} />
                 <Route path='chip' element={<ChipExample />} />
                 <Route path='textField' element={<TextFieldExample />} />
@@ -58,7 +53,6 @@ function App() {
                 <Route path='tabs' element={<TabsExample />} />
                 <Route path='table' element={<TableExample />} />
                 <Route path='toast' element={<ToastExample />} />
-                <Route path='api-test' element={<ApiTestExample />} />
               </Route>
               <Route path='test'>
                 <Route path='board' element={<Board />} />
