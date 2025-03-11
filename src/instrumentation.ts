e3a5bc231ae3e3d8a140b474dfad35a4845c97f3
Jay. (neocode24) <neocode24@gmail.com>
2025-03-11 14:32:19 +0900

    - OpenTelemetry 설정 추가

---
+++ src/instrumentation.ts
@@ -0,0 +1,70 @@
+import { Resource } from '@opentelemetry/resources';
+import {
+    ATTR_SERVICE_NAME,
+} from '@opentelemetry/semantic-conventions';
+import {
+    // BatchSpanProcessor,
+    SimpleSpanProcessor,
+    WebTracerProvider,
+} from '@opentelemetry/sdk-trace-web'
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+// import { FetchInstrumentation } from "@opentelemetry/instrumentation-fetch";
+import { registerInstrumentations } from "@opentelemetry/instrumentation";
+import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
+import { ZoneContextManager } from '@opentelemetry/context-zone';
+// import { DocumentLoadInstrumentation } from "@opentelemetry/instrumentation-document-load";
+
+const setupOTelSDK = () => {
+    const resource = Resource.default().merge(
+        new Resource({
+            [ATTR_SERVICE_NAME]: "koko-fe",
+        }),
+    );
+
+    const traceExporter = new OTLPTraceExporter({
+        // url: "http://localhost:4318/v1/traces",
+        url: "https://" + import.meta.env.VITE_HOST + "/v1/traces",
+        headers: {},
+    });
+
+    // const spanProcessor = new BatchSpanProcessor(traceExporter);
+    const spanProcessor = new SimpleSpanProcessor(traceExporter);
+
+    const tracerProvider = new WebTracerProvider({
+        resource: resource,
+        spanProcessors: [spanProcessor],
+    });
+
+    tracerProvider.register({
+        contextManager: new ZoneContextManager(),
+    });
+
+    registerInstrumentations({
+        instrumentations: [
+            getWebAutoInstrumentations({
+                '@opentelemetry/instrumentation-document-load': {
+                    enabled: false,
+                },
+                '@opentelemetry/instrumentation-user-interaction': {
+                    enabled: false,
+                },
+                '@opentelemetry/instrumentation-fetch': {
+                    enabled: true,
+                    propagateTraceHeaderCorsUrls: /.*/,
+                    clearTimingResources: true,
+                },
+                '@opentelemetry/instrumentation-xml-http-request': {
+                    enabled: true,
+                    applyCustomAttributesOnSpan(span, xhr) {
+                        const method = (span as any).attributes["http.method"]?.toString() || "UNKNOWN";
+                        const urlObj = new URL(xhr.responseURL);
+                        const path = urlObj.pathname;
+                        span.updateName(`${method} ${path}`);
+                    },
+                },
+            }),
+        ],
+    });
+};
+
+export { setupOTelSDK };
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import './opentelemetry';
+import { setupOTelSDK } from "./instrumentation.ts";
 import './index.css';
+
+if (!import.meta.env.DEV) {
+    setupOTelSDK();
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
