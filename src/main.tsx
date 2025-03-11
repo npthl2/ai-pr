e2218936ed909be1abe0e3588d1e7f3d3d9bf83b
Jay. (neocode24) <neocode24@gmail.com>
2025-03-11 14:38:45 +0900

    - Dockerize - OpenTelemetry Agent 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import { setupOTelSDK } from "./instrumentation.ts";
+import { setupOTelSDK } from './instrumentation.ts';
 import './index.css';
 
 if (!import.meta.env.DEV) {
-    setupOTelSDK();
+  setupOTelSDK();
 }
 
 createRoot(document.getElementById('root')!).render(
