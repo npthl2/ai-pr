1001e3430634f03c45baa563c51625c9bb949cf2
jiyeon <jiyeon.nam@kt.com>
2025-03-13 20:37:28 +0900

    [KAN-38] 잘못들어간 파일 수정

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,6 @@ import Board from '@pages/test/board/Board';
 import RegistBoard from '@pages/test/board/component/RegistBoard';
 import MemoAndHistoryPanel from '@pages/memoAndSendHistory/MemoAndSendHistoryPanel';
 import Login from '@pages/auth/Login';
-import ContractSectionComponent from '@pages/registration/sections/contract/ContractSectionComponent';
 
 function App() {
   return (
@@ -58,10 +57,6 @@ function App() {
               <Route path='test'>
                 <Route path='board' element={<Board />} />
                 <Route path='board/regist' element={<RegistBoard />} />
-                <Route
-                  path='additional-service'
-                  element={<ContractSectionComponent contractTabId='test-tab-id' />}
-                />
               </Route>
               <Route element={<ProtectedRoute />}>
                 <Route element={<MainLayout />}>
