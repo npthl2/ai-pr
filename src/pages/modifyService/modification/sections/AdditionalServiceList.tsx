05c2d5eaf7306e39def214d7ee6c24a5e290b58d
개상 <kyleee0522@gmail.com>
2025-04-01 16:02:02 +0900

    tooltip 위치 조정

--- src/pages/modifyService/modification/sections/AdditionalServiceList.tsx
+++ src/pages/modifyService/modification/sections/AdditionalServiceList.tsx
@@ -270,16 +270,18 @@ const AdditionalServiceList = ({
           </StyledTableBlankCell>
           <TableCell>
             <Box sx={{ width: '500px', alignItems: 'left' }}>
-              <TextContainer>
-                {isRestricted && (
-                  <Tooltip title={getRestrictionMessage(service)} arrow placement='bottom'>
-                    <EllipsisTypography data-testid={`service-name-${service.serviceId}`}>
-                      {service.serviceName}
-                    </EllipsisTypography>
-                  </Tooltip>
-                )}
-                {!isRestricted && <EllipsisTypography>{service.serviceName}</EllipsisTypography>}
-              </TextContainer>
+              <Box display='flex'  alignItems='left'>
+                <TextContainer>
+                  {isRestricted && (
+                    <Tooltip title={getRestrictionMessage(service)} arrow placement='bottom'>
+                      <EllipsisTypography data-testid={`service-name-${service.serviceId}`}>
+                        {service.serviceName}
+                      </EllipsisTypography>
+                    </Tooltip>
+                  )}
+                  {!isRestricted && <EllipsisTypography>{service.serviceName}</EllipsisTypography>}
+                </TextContainer>
+              </Box>
             </Box>
           </TableCell>
 
