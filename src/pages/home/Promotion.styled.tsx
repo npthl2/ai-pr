b29f3b1dcd25b9e69c7b3bcfe378dd5fe2ad5e9a
myeongjin-kane <myeongjin@lgcns.com>
2025-04-03 14:32:28 +0900

    공유용 홈 Layout

---
+++ src/pages/home/Promotion.styled.tsx
@@ -0,0 +1,8 @@
+import { Box, styled } from '@mui/material';
+
+export const PromotionContainer = styled(Box)(() => ({
+  display: 'flex',
+  height: '303px',
+  flexDirection: 'column',
+  gap: '12px',
+}));
