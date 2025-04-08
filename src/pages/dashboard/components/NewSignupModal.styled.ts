61d56094d98a4578e0431d77cfbaf200e0f8bc57
jiyeon <jiyeon.nam@kt.com>
2025-04-08 17:22:50 +0900

    KAN-326: 불필요한 파일 삭제

--- src/pages/dashboard/components/NewSignupModal.styled.ts
+++
@@ -1,9 +0,0 @@
-import { Box } from '@mui/material';
-import { styled } from '@mui/material/styles';
-
-export const ContentContainer = styled(Box)({
-  display: 'flex',
-  flexDirection: 'column',
-  gap: '24px',
-  padding: '16px',
-});
